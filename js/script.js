af0598180a402058e34628896c36a78962b4eea1
KornHolio216 <machs1337@gmail.com>
2020-07-31 16:25:01 +0200

    dec2bin

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
+//bin2dec
 const bin2dec = document.querySelector(".bin2dec")
 
 bin2dec.addEventListener("change", ConvertBin2Dec)
 
-function ConvertBin2Dec(params) {
+function ConvertBin2Dec() {
     const bin = document.querySelector(".binInput").value
 
     if (bin === "") return alert("The field cannot be empty!")
     bin.split('').map((char) => {
-        if (char !== '0' && char !== '1') return alert('Please, enter a binary number');
+        if (char !== '0' && char !== '1'){
+            return alert('Please, enter a binary number');
+        } 
     });
     const dec = parseInt(bin, 2)
     document.querySelector(".decOutput").value = dec
     return true
 }
 
+//dec2bin
+
+const dec2bin = document.querySelector(".dec2bin")
+
+dec2bin.addEventListener("change", ConvertDec2Bin)
+
+function ConvertDec2Bin() {
+    const dec = document.querySelector(".decInput").value
+
+    if(dec===""){
+        return alert("The field cannot be empty!")
+    }
+
+    const bin = parseInt(dec, 10).toString(2)
+
+    document.querySelector(".binOutput").value = bin
+}
