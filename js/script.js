94db98679d2bd22ea252e126ff462855713c898b
KornHolio216 <machs1337@gmail.com>
2020-07-31 17:31:05 +0200

    js comments and bg change

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 //bin2dec
+
 const bin2dec = document.querySelector(".bin2dec")
 
+//adding  event listener for changes to the form
 bin2dec.addEventListener("change", ConvertBin2Dec)
 
 function ConvertBin2Dec() {
     const bin = document.querySelector(".binInput").value
 
-    if (bin === "") return alert("The field cannot be empty!")
+      //making sure field is not empty
+    if (bin === ""){
+    return alert("The field cannot be empty!") 
+    }
+    //checking string contain an number other than 0 or 1
     bin.split('').map((char) => {
         if (char !== '0' && char !== '1'){
             return alert('Please, enter a binary number');
         } 
     });
+    //chaning binary number into decimal number
     const dec = parseInt(bin, 2)
+    //changing input value
     document.querySelector(".decOutput").value = dec
     return true
 }
@@ -21,16 +29,18 @@ function ConvertBin2Dec() {
 
 const dec2bin = document.querySelector(".dec2bin")
 
+//adding  event listener for changes to the form
 dec2bin.addEventListener("change", ConvertDec2Bin)
 
 function ConvertDec2Bin() {
     const dec = document.querySelector(".decInput").value
 
+     //making sure field is not empty
     if(dec===""){
         return alert("The field cannot be empty!")
     }
-
+     //chaning decimal number into binary number
     const bin = parseInt(dec, 10).toString(2)
-
+        //changing input value
     document.querySelector(".binOutput").value = bin
 }
